3655ffbc06f62699b5cf3da0a06376ac7bdb1298
n0sc3tipsum <da322@ic.ac.uk>
2026-02-25 13:05:11 +0000

    init package

---
+++ src/qpe/solver/__init__.py
@@ -0,0 +1,36 @@
+"""
+Solver package initialization : registers all built-in solvers.
+
+To add a new solver:
+  1. Implement the QuantizationSolver protocol in a new module
+  2. Create a SolverConfig subclass with your parameters
+  3. Register it here with SolverFactory.register()
+  
+"""
+from .protocol import SolverFactory
+from .models import (
+    SolverInput, 
+    SolverOutput, 
+    LayerAssignment, 
+    LayerDescriptor
+)
+from .config import SolverConfig
+
+__all__ = [
+    "SolverFactory",
+    "SolverInput",
+    "SolverOutput",
+    "SolverConfig",
+    "LayerAssignment", 
+    "LayerDescriptor"
+]
+
+
+
+from optimizers.quality import ILPQualityMinimizer
+from optimizers.resources import ILPResourceMinimizer
+from optimizers.pareto_explorer import ParetoExplorer
+
+SolverFactory.register("quality_minimizer", ILPQualityMinimizer)
+SolverFactory.register("resource_minimizer", ILPResourceMinimizer)
+SolverFactory.register("pareto_explorer", ParetoExplorer)
