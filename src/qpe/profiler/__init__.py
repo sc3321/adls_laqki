d9b334d5dc4c9617c5e5807752675d991ba7f5d6
n0sc3tipsum <da322@ic.ac.uk>
2026-03-05 15:11:53 +0000

    init m8

--- src/qpe/profiler/__init__.py
+++ src/qpe/profiler/__init__.py
@@ -1,11 +1,13 @@
 from .gpu_specs import GPUSpec
 from .layer_profiler import LayerProfiler
+from .models import ModelProfileResult
 from benchmark.serving_benchmark import ServingBenchmark
 from benchmark.models import ServingBenchmarkConfig, ServingBenchmarkResult
 
 __all__ = [
     "GPUSpec",
     "LayerProfiler",
+    "ModelProfileResult",
     "ServingBenchmark", 
     "ServingBenchmarkConfig", 
     "ServingBenchmarkResult"
